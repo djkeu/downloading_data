728a1cf6727f0311ab54bb27b4eb19834f75279e
djkeu <mrkooij@gmail.com>
2023-10-27 18:31:31 +0200

    Import and use csv instead of json

--- world_fires.py
+++ world_fires.py
@@ -1,5 +1,9 @@
-import json
+import csv
 
 from plotly.graph_objs import Layout
 from plotly import offline
 
+filename = 'data/world_fires_7_day.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
