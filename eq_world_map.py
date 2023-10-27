957245f0a12c721cc1e8aea59bf26b496aacc585
djkeu <mrkooij@gmail.com>
2023-10-27 14:06:33 +0200

    Add hover texts

--- eq_world_map.py
+++ eq_world_map.py
@@ -14,14 +14,16 @@ with open(readable_file, 'w') as f:
 
 all_eq_dicts = all_eq_data['features']
 
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(title)
 
 # print(mags[:10])
 # print(lons[:5])
@@ -33,6 +35,7 @@ data = [{
     'type': 'scattergeo',
     'lon': lons,
     'lat': lats,
+    'text': hover_texts,
     'marker': {
         'size': [5*mag for mag in mags],
         'color': mags,
