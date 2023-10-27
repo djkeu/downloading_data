a210a2cdd923a71a0a1a514e4ee3e35219389a71
djkeu <mrkooij@gmail.com>
2023-10-27 18:33:43 +0200

    Print indexes and column headers

--- world_fires.py
+++ world_fires.py
@@ -7,3 +7,8 @@ filename = 'data/world_fires_7_day.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
+
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
+
+    
