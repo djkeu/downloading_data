6bc28a9fe660d4007f841ddd650944b36196ad2b
djkeu <mrkooij@gmail.com>
2023-10-27 16:38:00 +0200

    Rename all_day.json

--- recent_earthquakes.py
+++ recent_earthquakes.py
@@ -3,3 +3,10 @@ import json
 from plotly.graph_objs import Layout
 from plotly import offline
 
+filename = 'data/all_day.json'
+with open(filename) as f:
+    all_data = json.load(f)
+
+eq_dicts = all_data['features']
+
+# Can't json.load downloaded .json file
